2e5f2d8e8873a89b33509270fd12b6abe0ce1497
Valerio Farina <valerio.farina.1994@gmail.com>
2020-10-14 16:12:39 +0200

    Creato algoritmo per determinare il vincitore

--- main.js
+++ main.js
@@ -7,9 +7,18 @@
 // genero un numero casuale da 1 a 6 per simulare il lancio del primo giocatore
 var rollDice1 = Math.floor(Math.random() * 6) + 1;
 
-console.log('Esito lancio giocatore1: ' + rollDice1);
-
 // genero un numero casuale da 1 a 6 per simulare il lancio del secondo giocatore
 var rollDice2 = Math.floor(Math.random() * 6) + 1;
 
+// comunico gli esiti dei lanci
+console.log('Esito lancio giocatore1: ' + rollDice1);
 console.log('Esito lancio giocatore2: ' + rollDice2);
+
+// controllo chi ha vinto: vince chi ottiene dal lancio un numero maggiore
+if (rollDice1 > rollDice2) {
+    console.log('Ha vinto il giocatore1 !!!');
+} else if (rollDice2 > rollDice1) {
+    console.log('Ha vinto il giocatore2 !!!');
+} else {
+    console.log('Patta !!!');
+}
