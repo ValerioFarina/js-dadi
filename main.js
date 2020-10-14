8bf7711725c1c3be99f7bdeec7dffe1702df8951
Valerio Farina <valerio.farina.1994@gmail.com>
2020-10-14 16:04:57 +0200

    Generato numero casuale da 1 a 6 per simulare il lancio del primo giocatore

---
+++ main.js
@@ -0,0 +1,10 @@
+/*
+    - Generare un numero casuale tra 1 e 6 per simulare il lancio di un dado (giocata 1).
+    - Generare un secondo numero casuale tra 1 e 6, che sarà la seconda giocata.
+    - Comunicare quali dei due utenti (tra giocatore 1 e giocatore 2) ha vinto.
+*/
+
+// genero un numero casuale da 1 a 6 per simulare il lancio del primo giocatore
+var rollDice1 = Math.floor(Math.random() * 6) + 1;
+
+console.log(rollDice1); 
